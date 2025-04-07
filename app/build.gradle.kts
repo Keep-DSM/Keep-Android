269eee79830338d2f27fd186655529f1cf42509f
uiel <parkuiery@gmail.com>
2025-04-07 14:03:56 +0900

    chore :: 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,22 +1,37 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+    kotlin("plugin.serialization")
+    alias(libs.plugins.dagger.hilt)
+    id("com.google.gms.google-services")
+    alias(libs.plugins.google.devtools.ksp)
 }
 
 android {
     namespace = "com.uiel.keep"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.uiel.keep"
         minSdk = 28
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
+        }
+    }
+    flavorDimensions += "server"
+    productFlavors {
+        create("dev") {
+            dimension = "server"
+            //applicationIdSuffix = ".dev"
+        }
+        create("prod") {
+            dimension = "server"
         }
     }
 
@@ -28,6 +43,11 @@ android {
                 "proguard-rules.pro"
             )
         }
+
+        debug {
+            //applicationIdSuffix = ".debug"
+            isDebuggable = true
+        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
@@ -38,6 +58,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
@@ -66,4 +87,32 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation("androidx.navigation:navigation-compose:2.8.9")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.32.0")
+
+    implementation("org.orbit-mvi:orbit-core:9.0.0")
+    implementation("org.orbit-mvi:orbit-viewmodel:9.0.0")
+    implementation("org.orbit-mvi:orbit-compose:9.0.0")
+
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    ksp("com.google.dagger:hilt-android-compiler:2.51.1")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+    implementation("androidx.datastore:datastore-preferences:1.1.2")
+
+    implementation("com.airbnb.android:lottie-compose:6.6.3")
+
+    implementation(platform("com.google.firebase:firebase-bom:33.11.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-messaging:24.1.1")
+
+    implementation("com.blankj:utilcodex:1.31.1")
+
+    implementation("com.datadoghq:dd-sdk-android-rum:2.19.2")
+
+    implementation(project(":core:kds"))
 }
