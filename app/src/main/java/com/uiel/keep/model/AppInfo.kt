86d1901019f54ae78c09f834808ad21f313deb10
uiel <parkuiery@gmail.com>
2025-04-07 14:00:07 +0900

    feat :: AppInfo model 구현

---
+++ app/src/main/java/com/uiel/keep/model/AppInfo.kt
@@ -0,0 +1,10 @@
+package com.uiel.keep.model
+
+import android.graphics.drawable.Drawable
+
+data class AppInfo(
+    val isChecked: Boolean,
+    val packageName: String,
+    val appName: String,
+    val appIcon: Drawable,
+)
