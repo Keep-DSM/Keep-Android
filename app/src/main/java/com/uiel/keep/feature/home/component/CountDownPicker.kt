59be733b01f2726a70cb1729b1e628e8d2cec381
uiel <parkuiery@gmail.com>
2025-04-07 11:26:55 +0900

    feat :: CountDownPicker 구현

---
+++ app/src/main/java/com/uiel/keep/feature/home/component/CountDownPicker.kt
@@ -0,0 +1,104 @@
+package com.uiel.keep.feature.home.component
+
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.uiel.keep.Picker
+import com.uiel.keep.rememberPickerState
+import java.time.LocalTime
+
+@Composable
+fun CountDownPicker(
+    modifier: Modifier = Modifier,
+    onChangeCountdownTime: (LocalTime) -> Unit,
+) {
+    val hourValues = remember { (0..23).map { it.toString() } }
+    val minuteValues = remember { (0..59).map { it.toString() } }
+    val hourPickerState = rememberPickerState()
+    val minutePickerState = rememberPickerState()
+
+    LaunchedEffect(hourPickerState.selectedItem,minutePickerState.selectedItem) {
+        if(hourPickerState.selectedItem.isNotEmpty() && minutePickerState.selectedItem.isNotEmpty()) {
+            onChangeCountdownTime(LocalTime.of(hourPickerState.selectedItem.toInt(),minutePickerState.selectedItem.toInt()))
+        }
+    }
+
+    Box(
+        modifier = modifier.fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 4.dp)
+                .height(32.dp)
+                .background(
+                    shape = RoundedCornerShape(8.dp),
+                    color = Color(0xFF282831),
+                )
+        ) {
+
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            Picker(
+                modifier = Modifier.widthIn(min = 28.dp),
+                state = hourPickerState,
+                items = hourValues,
+                visibleItemsCount = 7,
+                color = Color.White,
+                textStyle = TextStyle(
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 18.sp,
+                ),
+                textModifier = Modifier.padding(vertical = 4.dp),
+            )
+            Text(
+                text = "시간",
+                fontWeight = FontWeight.Bold,
+            )
+            Spacer(modifier = Modifier.width(12.dp))
+            Picker(
+                modifier = Modifier.widthIn(min = 28.dp),
+                state = minutePickerState,
+                items = minuteValues,
+                visibleItemsCount = 7,
+                color = Color.White,
+                textStyle = TextStyle(
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 18.sp,
+                ),
+                textModifier = Modifier.padding(vertical = 4.dp),
+            )
+            Text(
+                text = "분",
+                fontWeight = FontWeight.Bold,
+            )
+        }
+    }
+
+}
