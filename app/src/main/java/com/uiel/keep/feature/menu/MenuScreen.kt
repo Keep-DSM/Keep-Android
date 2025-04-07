791eef8213ded1ebb3b236f8d43cf8e4c4a84821
uiel <parkuiery@gmail.com>
2025-04-07 14:02:08 +0900

    feat :: Menu 구현

---
+++ app/src/main/java/com/uiel/keep/feature/menu/MenuScreen.kt
@@ -0,0 +1,57 @@
+package com.uiel.keep.feature.menu
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import com.uiel.keep.R
+import com.uiel.keep.feature.menu.component.MenuItem
+import com.uiel.keep.util.isTestMode
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MenuScreen(
+    modifier: Modifier = Modifier,
+    onNavigateDevTool: () -> Unit,
+    onNavigateBack: () -> Unit,
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = { onNavigateBack() }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.baseline_arrow_back_ios_24),
+                            contentDescription = null,
+                            tint = Color(0xFFFE9E0B),
+                        )
+                    }
+                },
+                title = { /*TODO*/ }
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+        ) {
+            if (isTestMode()) {
+                MenuItem(
+                    icon = R.drawable.laptop,
+                    title = "개발자 옵션",
+                    onClick = onNavigateDevTool,
+                )
+            }
+        }
+    }
+}
