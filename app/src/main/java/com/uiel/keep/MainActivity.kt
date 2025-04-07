394c3b532b7c64cd4ddd110365577602afcc15b0
uiel <parkuiery@gmail.com>
2025-04-07 13:53:58 +0900

    feat :: MainActivity 구현

--- app/src/main/java/com/uiel/keep/MainActivity.kt
+++ app/src/main/java/com/uiel/keep/MainActivity.kt
@@ -1,47 +1,64 @@
 package com.uiel.keep
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.google.firebase.messaging.FirebaseMessaging
 import com.uiel.keep.ui.theme.KeepTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var deviceTokenManager: DeviceTokenManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            KeepTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            KeepTheme(darkTheme = true) {
+                val systemUiController = rememberSystemUiController()
+                systemUiController.setStatusBarColor(color = Color.Transparent, darkIcons = false)
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    contentWindowInsets = WindowInsets(0.dp),
+                ) { innerPadding ->
+                    KeepApp(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.d("MainActivity", task.exception.toString(), task.exception)
+                return@addOnCompleteListener
+            }
+            lifecycleScope.launch {
+                deviceTokenManager.saveDeviceToken(deviceToken = task.result)
+            }
+        }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     KeepTheme {
-        Greeting("Android")
+        KeepApp()
     }
 }
