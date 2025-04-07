04fda8f355a655757c82045097e66f4a4e25495f
uiel <parkuiery@gmail.com>
2025-04-07 11:23:59 +0900

    feat :: DevTool 구현

---
+++ app/src/main/java/com/uiel/keep/feature/devtool/DevToolNavigation.kt
@@ -0,0 +1,20 @@
+package com.uiel.keep.feature.devtool
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object DevToolRoute
+
+fun NavController.navigateToDevTool(
+    navOptions: NavOptions? = null
+) = navigate(route = DevToolRoute, navOptions = navOptions)
+
+fun NavGraphBuilder.devToolScreen() {
+    composable<DevToolRoute> {
+        DevToolScreen()
+    }
+}
---
+++ app/src/main/java/com/uiel/keep/feature/devtool/DevToolScreen.kt
@@ -0,0 +1,37 @@
+package com.uiel.keep.feature.devtool
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.uiel.keep.feature.devtool.component.DevToolItem
+import com.uiel.keep.util.deviceId
+import org.orbitmvi.orbit.compose.collectAsState
+
+@Composable
+fun DevToolScreen(
+    modifier: Modifier = Modifier,
+    viewModel: DevToolViewModel = hiltViewModel(),
+) {
+    val uiState by viewModel.collectAsState()
+
+    Scaffold(modifier = modifier.fillMaxSize()) { paddingValues ->
+        Column(
+            modifier = Modifier.fillMaxSize().padding(paddingValues)
+        ) {
+            DevToolItem(
+                title = "기기 ID",
+                content = deviceId(),
+            )
+            DevToolItem(
+                title = "fcm 토큰",
+                content = uiState.fcmToken,
+            )
+        }
+    }
+}
