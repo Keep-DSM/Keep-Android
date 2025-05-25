5c9c93464b22ba32a4649031bd79aca959a1463f
uiel <parkuiery@gmail.com>
2025-05-25 20:57:04 +0900

    feat ::  KeepCheckbox 구현

---
+++ core/kds/src/main/java/com/uiery/kds/KeepCheckbox.kt
@@ -0,0 +1,35 @@
+package com.uiery.kds
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxColors
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.uiery.kds.theme.KeepTheme
+
+@Composable
+fun KeepCheckbox(
+    checked: Boolean,
+    onCheckedChange: ((Boolean) -> Unit)?,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    colors: CheckboxColors = CheckboxDefaults.colors(
+        checkedColor = Color.Blue,
+        checkmarkColor = Color.White,
+        uncheckedColor = KeepTheme.colors.onTertiaryContainer,
+    ),
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
+) {
+    Checkbox(
+        checked = checked,
+        onCheckedChange = onCheckedChange,
+        modifier = modifier,
+        enabled = enabled,
+        colors = colors,
+        interactionSource = interactionSource,
+    )
+}
+
