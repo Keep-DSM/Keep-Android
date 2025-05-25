3bcda103d020f36e3736c49422c759fdbe30f3ec
uiel <parkuiery@gmail.com>
2025-05-25 20:57:32 +0900

    feat :: KeepModalBottomSheet 구현

---
+++ core/kds/src/main/java/com/uiery/kds/KeepModalBottomSheet.kt
@@ -0,0 +1,71 @@
+package com.uiery.kds
+
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.ModalBottomSheetDefaults
+import androidx.compose.material3.ModalBottomSheetProperties
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.SheetValue.Hidden
+import androidx.compose.material3.contentColorFor
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Dp
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.uiery.kds.theme.KeepTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun KeepModalBottomSheet(
+    onDismissRequest: () -> Unit,
+    modifier: Modifier = Modifier,
+    sheetState: SheetState = rememberModalBottomSheetState(),
+    sheetMaxWidth: Dp = BottomSheetDefaults.SheetMaxWidth,
+    shape: Shape = BottomSheetDefaults.ExpandedShape,
+    containerColor: Color = KeepTheme.colors.onSecondary,
+    contentColor: Color = contentColorFor(containerColor),
+    tonalElevation: Dp = BottomSheetDefaults.Elevation,
+    scrimColor: Color = BottomSheetDefaults.ScrimColor,
+    dragHandle: @Composable (() -> Unit)? = { BottomSheetDefaults.DragHandle(color = KeepTheme.colors.tertiaryContainer) },
+    windowInsets: WindowInsets = BottomSheetDefaults.windowInsets,
+    properties: ModalBottomSheetProperties = ModalBottomSheetDefaults.properties(),
+    content: @Composable ColumnScope.() -> Unit,
+) {
+    val systemUiController = rememberSystemUiController()
+    val navigationBarColor = KeepTheme.colors.onSecondary
+    val statusBarColor = BottomSheetDefaults.ScrimColor
+    val backgroundColor = KeepTheme.colors.background
+
+    LaunchedEffect(sheetState.targetValue) {
+        if(sheetState.targetValue != Hidden) {
+            with(systemUiController) {
+                setStatusBarColor(statusBarColor)
+                setNavigationBarColor(navigationBarColor)
+            }
+        } else {
+            systemUiController.setSystemBarsColor(backgroundColor)
+        }
+    }
+
+    ModalBottomSheet(
+        onDismissRequest = onDismissRequest,
+        modifier = modifier,
+        sheetState = sheetState,
+        sheetMaxWidth = sheetMaxWidth,
+        shape = shape,
+        containerColor = containerColor,
+        contentColor = contentColor,
+        tonalElevation = tonalElevation,
+        scrimColor = scrimColor,
+        dragHandle = dragHandle,
+        windowInsets = windowInsets,
+        properties = properties,
+        content = content,
+    )
+}
